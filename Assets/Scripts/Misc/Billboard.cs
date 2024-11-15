43154e1a91a52fc7515cade7b7372aef6a28f11e
dhruv304c2 <62604491+dhruv304c2@users.noreply.github.com>
2024-11-15 14:51:39 +0530

    removing release prompt because over controller

--- Assets/Scripts/Misc/Billboard.cs
+++ Assets/Scripts/Misc/Billboard.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour {
     public Camera targetCamera;
+    [SerializeField] bool maintainAngleOfVision;
+    [Header("This is not an actaul angle but an arbitrary value corresponding to it")]
+    [SerializeField] float angleOfVision;
+
+    Vector3 originalScale;
 
     void Start() {
         if (targetCamera == null) {
             targetCamera = Camera.main;
         }
+
+        originalScale = transform.localScale;
 
         Observable.EveryLateUpdate()
             .TakeUntilDestroy(this)
@@ -19,6 +26,12 @@ public class Billboard : MonoBehaviour {
 
         var target= targetCamera.transform.position;
         transform.LookAt(2 * transform.position - target);
+
+        if(maintainAngleOfVision) {
+            var distance = Vector3.Distance(transform.position, target);
+            var scale = - Mathf.Tan(angleOfVision) * distance;
+            transform.localScale = originalScale * scale;
+        }
     }
 }
 
